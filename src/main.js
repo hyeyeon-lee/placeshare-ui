27782854a02e8584655f0cc958ce25763406a26a
hyeyeon-lee <dear.yeonee@gmail.com>
2020-10-19 22:21:55 +0900

    vue router 적용
    helloworld 컴포넌트를 사용하여 router 적용 연습

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.ues(VueRouter);
 
 Vue.config.productionTip = false
 
+import HelloWorld from './components/HelloWorld.vue'
+
+const routes = [
+  { path: '/hello', component: HelloWorld },
+]
+
+const router = new VueRouter({
+  routes
+})
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
