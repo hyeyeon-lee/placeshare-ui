df9bb421b65c780e7ad18cd4ae6d02a6b327c244
hyeyeon-lee <dear.yeonee@gmail.com>
2020-10-24 00:12:04 +0900

    feat: 로그인 화면 추가

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,26 +1,36 @@
 <template>
-  <div class="login">
-    <h1>로그인 화면입니다.</h1>
-    id: <input type="text" v-model="id">
-    password: <input type="password" v-model="password">
+  <div>
+    <h2>로그인</h2>
+    <h5>place share에 오신 것을 환영합니다 !</h5>
+    <div>아이디 <input type="text" v-model="id"/> </div>
+    <div>비밀번호 <input type="password" v-model="password"/></div>
+    <input type="button" value="로그인" @click="goMain"/> 
+
+    <p><a href="#">아이디 찾기</a></p>
+    <p><a href="#">비밀번호 찾기</a></p>
+
+    <p>아직 회원이 아니신가요?</p>
+    <input type="button" value="회원가입" />
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'Login',
-  props: {
-    msg: String
-  },
   data() {
     return {
       id: 'abc',
       password: 'abc'
     } 
   },
+  methods: {
+    goMain() {
+      this.$router.push("hello");
+    }
+  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
